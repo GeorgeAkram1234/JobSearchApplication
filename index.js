79f6ebde6c1b72da198aae5c7bec28296184e8c2
joka <jokaakram@gmail.com>
2025-03-01 20:52:03 +0200

    prod

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { bootstrap } from './src/app.controller.js'
 import dotenv from 'dotenv'
 import path from 'node:path'
 import { runIo } from './src/modules/socket/socket.controller.js'
-dotenv.config({ path: path.resolve('./src/config/.env.dev') })
+dotenv.config({ path: path.resolve('./src/config/.env.prod') })
 const app = express()
 const port = process.env.PORT
 
